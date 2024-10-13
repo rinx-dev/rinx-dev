b147f77c3936f4f7a2ad67e30e94063ecc3ad825
rinx-dev <rinxdev2023@gmail.com>
2024-10-13 18:49:32 +0700

    -

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,12 +7,13 @@ import sitemap from "@astrojs/sitemap";
 // https://astro.build/config
 export default defineConfig({
   output: "server",
+  site: "https://rinx.dev",
+  base: "/",
   adapter: cloudflare({
     platformProxy: {
       enabled: true,
     },
   }),
-  site: "https://rinx.dev",
   integrations: [
     sitemap({
       lastmod: new Date("2024-10-08"), // YYYY-MM-DD
